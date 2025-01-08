051854f3c42e1e84f6f115121a0bb83507964b45
DHEERAJ PRATAP SINGH <72618058+mr-dheeraj@users.noreply.github.com>
2025-01-08 18:57:01 +0530

    product-slider product-cards

--- script.js
+++ script.js
@@ -36,3 +36,13 @@ next_btn.addEventListener('click', (e)=>
         changeSlide();
     
     })
+
+
+
+const scrollContainer=document.querySelectorAll('.products');
+for (const item of scrollContainer) {
+    item.addEventListener('wheel',(evt)=>{
+        evt.preventDefault();
+        item.scrollLeft+=evt.deltaY;
+    })
+}
